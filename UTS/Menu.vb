c7456db32ba7243a596f759d13019d1333a295fa
mlyandhva <amliandhv@gmail.com>
2026-03-31 17:43:02 +0700

    Delete UTS directory

--- UTS/Menu.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class Menu
-    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        Dim formReservasi As New Reservasi()
-        formReservasi.Show()
-        Me.Hide()
-    End Sub
-End Class
